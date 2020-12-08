08121c02e77dbfe02cad18647ec2f6b154894980
ambrt <ambrth@gmail.com>
2020-12-08 17:17:32 +0100

    settings and copy tags to new note

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,28 @@ function escapeTitleText(text: string) {
 
 joplin.plugins.register({
 	onStart: async function () {
+
+		await joplin.settings.registerSection('convertTextToNewNoteSection', {
+			label: 'Convert to New Note',
+			iconName: 'fas fa-star',
+		});
+		await joplin.settings.registerSetting('converTextToNewNoteSettingsBacklink', {
+			value: true,
+			type: 3,
+			section: 'convertTextToNewNoteSection',
+			public: true,
+			label: 'Include backlink in new note',
+		});
+
+		await joplin.settings.registerSetting('converTextToNewNoteSettingsCopyTags', {
+			value: true,
+			type: 3,
+			section: 'convertTextToNewNoteSection',
+			public: true,
+			label: 'Copy tags to new note',
+		});
+
+
 		await joplin.commands.register({
 			name: 'convertTextToNewNote',
 			label: 'Convert text to new note',
@@ -21,18 +43,59 @@ joplin.plugins.register({
 
 				// Get active folder
 				const folder = await joplin.data.get(['folders', note.parent_id]);
+
+				//Construct new note
 				let backReference = `from [${escapeTitleText(note.title)}](:/${note.id})`
-				let body = selectedText+"\n\n"+backReference;
+
+				//Get option
+				const isBacklink = await joplin.settings.value('converTextToNewNoteSettingsBacklink');
+				const isCopyTags = await joplin.settings.value('converTextToNewNoteSettingsCopyTags');
+
+
+				let body = selectedText + "\n\n"
+
+				if (isBacklink) {
+					body = body + backReference
+				}
+
 				let title = selectedText.split('\n')[0];
 
+
+
+				//Create new note
 				let newnote = await joplin.data.post(['notes'], null, { body: body, title: title, parent_id: folder.id });
+
+				//Get tags
+				if (isCopyTags) {
+					console.log('getting tags')
+
+
+
+					let page = 1
+					let tags
+					let tagId
+					let has_more = true
+					while (has_more) {
+						tags = await joplin.data.get(['notes', note.id, 'tags'], { page: page });
+						tags.items.forEach(async element => {
+							console.log(element)
+							console.log(newnote.id)
+							await joplin.data.post(['tags',element.id, 'notes'],null,{id:newnote.id});
+						});
+						if (tags.has_more) { page = page + 1 } else { has_more = false }
+
+					}
+				}
+
+
+				//Create reference to new note
 				await joplin.commands.execute('replaceSelection', `[${escapeTitleText(title)}](:/${newnote.id})`);
 
 
 			}
 		})
-		await joplin.views.toolbarButtons.create('convertToNewNoteViaToolbar','convertTextToNewNote', ToolbarButtonLocation.EditorToolbar);
-		await joplin.views.menuItems.create('convertToNewNoteViaMenu','convertTextToNewNote',MenuItemLocation.EditorContextMenu)
+		await joplin.views.toolbarButtons.create('convertToNewNoteViaToolbar', 'convertTextToNewNote', ToolbarButtonLocation.EditorToolbar);
+		await joplin.views.menuItems.create('convertToNewNoteViaMenu', 'convertTextToNewNote', MenuItemLocation.EditorContextMenu)
 	}
 
 });
