e7dd146e37318dd0e44351f5231f543e35f9788e
ambrt <ambrth@gmail.com>
2021-01-21 16:31:06 +0100

    switch to new, insert title

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,9 @@ function escapeTitleText(text: string) {
 
 joplin.plugins.register({
 	onStart: async function () {
-
+		const dialogs = joplin.views.dialogs;
+		const handle3= await dialogs.create('myDialog3');
+		
 		await joplin.settings.registerSection('convertTextToNewNoteSection', {
 			label: 'Convert to New Note',
 			iconName: 'fas fa-star',
@@ -29,6 +31,21 @@ joplin.plugins.register({
 			label: 'Copy tags to new note',
 		});
 
+		await joplin.settings.registerSetting('converTextToNewNoteSettingsAskForTitle', {
+			value: false,
+			type: 3,
+			section: 'convertTextToNewNoteSection',
+			public: true,
+			label: 'Ask for title if two or more lines are selected ',
+		});
+
+		await joplin.settings.registerSetting('converTextToNewNoteSettingsGoToNew', {
+			value: true,
+			type: 3,
+			section: 'convertTextToNewNoteSection',
+			public: true,
+			label: 'Switches to newly created note',
+		});
 
 		await joplin.commands.register({
 			name: 'convertTextToNewNote',
@@ -44,25 +61,61 @@ joplin.plugins.register({
 				// Get active folder
 				const folder = await joplin.data.get(['folders', note.parent_id]);
 
-				//Construct new note
-				let backReference = `from [${escapeTitleText(note.title)}](:/${note.id})`
+
 
 				//Get option
 				const isBacklink = await joplin.settings.value('converTextToNewNoteSettingsBacklink');
 				const isCopyTags = await joplin.settings.value('converTextToNewNoteSettingsCopyTags');
+				const isDialog = await joplin.settings.value('converTextToNewNoteSettingsAskForTitle')
+				const isSwitchToNew = await joplin.settings.value('converTextToNewNoteSettingsGoToNew')
 
-
+				//Construct new note
+				let backReference = `from [${escapeTitleText(note.title)}](:/${note.id})`
 				let body = selectedText + "\n\n"
-
 				if (isBacklink) {
 					body = body + backReference
 				}
 
-				let title = selectedText.split('\n')[0];
+				let title
+				let createOrNot = false
+				if (selectedText.split('\n').length == 1) {
+					title = selectedText.split('\n')[0];
+
+				} else if (isDialog) {
+					
+
+					title = body.split('\n')[0];
+					let cacheBust = Math.random();
+					await dialogs.setHtml(handle3, `
+		<p>Provide title</p>
+		<form name="titleForm">
+					<input type="hidden" name="rand" value='${cacheBust}'/>
+			<input type="text" value="${title.replace(/\"/g, '\'')}" name="title"/>
+		</form>
+		`);
+
+					let result3 = await dialogs.open(handle3);
+
+
+					title = result3.formData.titleForm.title
+					if(result3.id=="cancel"){
+						createOrNot = false
+					} else{
+						createOrNot = true
+					let bodyArr = body.split("\n")
+					bodyArr.splice(0, 0, title)
+					body = bodyArr.join("\n")
+				}
+
+				} else {
+					title = selectedText.split('\n')[0];
+				}
+
 
 
 
 				//Create new note
+				if(createOrNot){
 				let newnote = await joplin.data.post(['notes'], null, { body: body, title: title, parent_id: folder.id });
 
 				//Get tags
@@ -80,7 +133,7 @@ joplin.plugins.register({
 						tags.items.forEach(async element => {
 							console.log(element)
 							console.log(newnote.id)
-							await joplin.data.post(['tags',element.id, 'notes'],null,{id:newnote.id});
+							await joplin.data.post(['tags', element.id, 'notes'], null, { id: newnote.id });
 						});
 						if (tags.has_more) { page = page + 1 } else { has_more = false }
 
@@ -90,13 +143,16 @@ joplin.plugins.register({
 
 				//Create reference to new note
 				await joplin.commands.execute('replaceSelection', `[${escapeTitleText(title)}](:/${newnote.id})`);
-
+				if (isSwitchToNew) {
+					await joplin.commands.execute('openNote', newnote.id);
+				}
+			}
 
 			}
 		})
 		// add accelerator
 		await joplin.views.toolbarButtons.create('convertToNewNoteViaToolbar', 'convertTextToNewNote', ToolbarButtonLocation.EditorToolbar);
-		await joplin.views.menuItems.create('convertToNewNoteViaMenu', 'convertTextToNewNote', MenuItemLocation.EditorContextMenu, {accelerator:"Ctrl+Alt+N"})
+		await joplin.views.menuItems.create('convertToNewNoteViaMenu', 'convertTextToNewNote', MenuItemLocation.EditorContextMenu, { accelerator: "Ctrl+Alt+N" })
 	}
 
 });
